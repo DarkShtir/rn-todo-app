52ac82b0ea64c0ca7cea3b649c1f9e711d9cceb5
Drew Muliou <Andreyjedai@tut.by>
2020-04-16 17:32:14 +0300

    Final version of application, added request on backend, loader, and error handler

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -2,19 +2,42 @@ import React, { useReducer, useContext } from 'react';
 import { Alert } from 'react-native';
 import { TodoContext } from './todoContext';
 import { todoReducer } from './todoReducer';
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types';
+import {
+	ADD_TODO,
+	REMOVE_TODO,
+	UPDATE_TODO,
+	SHOW_LOADER,
+	HIDE_LOADER,
+	SHOW_ERROR,
+	CLEAR_ERROR,
+	FETCH_TODOS,
+} from '../types';
 import { ScreenContext } from '../screen/screenContext';
+import { Http } from '../../http';
 
 export const TodoState = ({ children }) => {
 	const initialState = {
-		todos: [{ id: '1', title: 'Create application' }],
+		todos: [],
+		loading: false,
+		error: null,
 	};
 
 	const { changeScreen } = useContext(ScreenContext);
 
 	const [state, dispatch] = useReducer(todoReducer, initialState);
 
-	const addTodo = (title) => dispatch({ type: ADD_TODO, title });
+	const addTodo = async (title) => {
+		clearError();
+		try {
+			const data = await Http.post(
+				'https://rn-todo-app-2539c.firebaseio.com/todos.json',
+				{ title }
+			);
+			dispatch({ type: ADD_TODO, title, id: data.name });
+		} catch (error) {
+			showError('Something went wrong. Try again!');
+		}
+	};
 
 	const removeTodo = (id) => {
 		const todo = state.todos.find((t) => t.id === id);
@@ -29,9 +52,16 @@ export const TodoState = ({ children }) => {
 				{
 					text: 'Remove',
 					style: 'destructive',
-					onPress: () => {
+					onPress: async () => {
 						changeScreen(null);
-						dispatch({ type: REMOVE_TODO, id });
+						try {
+							await Http.delete(
+								`https://rn-todo-app-2539c.firebaseio.com/todos/${id}.json`
+							);
+							dispatch({ type: REMOVE_TODO, id });
+						} catch (error) {
+							showError('Something went wrong. Try again!');
+						}
 					},
 				},
 			],
@@ -39,15 +69,62 @@ export const TodoState = ({ children }) => {
 		);
 	};
 
-	const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+	const fetchTodos = async () => {
+		showLoader();
+		clearError();
+		try {
+			const data = await Http.get(
+				`https://rn-todo-app-2539c.firebaseio.com/todos.json`
+			);
+			if (!data) {
+				dispatch({ type: FETCH_TODOS, todos: [] });
+			} else {
+				const todos = Object.keys(data).map((key) => ({
+					...data[key],
+					id: key,
+				}));
+				dispatch({ type: FETCH_TODOS, todos });
+			}
+		} catch (error) {
+			showError('Something went wrong. Try again!');
+			console.log(error);
+		} finally {
+			hideLoader();
+		}
+	};
+
+	const updateTodo = async (id, title) => {
+		clearError();
+		try {
+			await Http.patch(
+				`https://rn-todo-app-2539c.firebaseio.com/todos/${id}.json`,
+				{ title }
+			);
+			dispatch({ type: UPDATE_TODO, id, title });
+		} catch (error) {
+			showError('Something went wrong. Try again!');
+			console.log(error);
+		}
+	};
+
+	const showLoader = () => dispatch({ type: SHOW_LOADER });
+
+	const hideLoader = () => dispatch({ type: HIDE_LOADER });
+
+	const showError = (error) => dispatch({ type: SHOW_ERROR, error });
+
+	const clearError = () => dispatch({ type: CLEAR_ERROR });
 
 	return (
 		<TodoContext.Provider
 			value={{
 				todos: state.todos,
+				loading: state.loading,
+				error: state.error,
 				addTodo,
 				removeTodo,
 				updateTodo,
+				fetchTodos,
 			}}
 		>
 			{children}
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,17 +1,30 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { StyleSheet, View, FlatList, Image, Dimensions } from 'react-native';
 import { AddTodo } from '../components/AddTodo';
 import { Todo } from '../components/Todo';
 import { THEME } from '../theme';
 import { TodoContext } from '../context/todo/todoContext';
 import { ScreenContext } from '../context/screen/screenContext';
+import { AppLoader } from '../components/ui/AppLoader';
+import { AppText } from '../components/ui/AppText';
+import { AppButton } from '../components/ui/AppButton';
 
 export const MainScreen = () => {
-	const { addTodo, todos, removeTodo } = useContext(TodoContext);
+	const { addTodo, todos, removeTodo, fetchTodos, loading, error } = useContext(
+		TodoContext
+	);
 	const { changeScreen } = useContext(ScreenContext);
 	const [deviceWidth, setDeviceWidth] = useState(
 		Dimensions.get('window').width - THEME.PADDING_HORISONTAL * 2
 	);
+
+	const loadTodos = useCallback(async () => {
+		await fetchTodos(), [fetchTodos];
+	});
+
+	useEffect(() => {
+		loadTodos();
+	}, []);
 
 	useEffect(() => {
 		const update = () => {
@@ -26,6 +39,19 @@ export const MainScreen = () => {
 			Dimensions.removeEventListener('change', update);
 		};
 	});
+
+	if (loading) {
+		return <AppLoader />;
+	}
+
+	if (error) {
+		return (
+			<View style={styles.center}>
+				<AppText style={styles.error}>{error}</AppText>
+				<AppButton onPress={loadTodos}>Repeat</AppButton>
+			</View>
+		);
+	}
 
 	let content = (
 		<View style={{ width: deviceWidth }}>
@@ -53,7 +79,6 @@ export const MainScreen = () => {
 	return (
 		<View>
 			<AddTodo onSubmit={addTodo} />
-
 			{content}
 		</View>
 	);
@@ -70,5 +95,14 @@ const styles = StyleSheet.create({
 		width: '100%',
 		height: '100%',
 		resizeMode: 'contain',
+	},
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	error: {
+		fontSize: 20,
+		color: THEME.DANGER_COLOR,
 	},
 });
